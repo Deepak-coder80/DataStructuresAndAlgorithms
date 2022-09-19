c2601fa10ef8f98b836e1522444a83ff01f98603
DEEPAK M S <deepakcoder80@gmail.com>
2022-09-19 16:02:14 +0530

    infix to postfix successfully implemented

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import Stack.StackApplications.Conversion;
+
 public class Main {
 
     public static void main(String args[]){
-        System.out.println("Hello World");
+        Conversion stackConvertions = new Conversion();
+        String infix = "a+b*(c^d-e)^(f+g*h)-i";
+
+        System.out.println("Given Infix Expression : "+infix);
+        String result = stackConvertions.infixToPostfix(infix);
+
+        if(result==null){
+            System.out.println("Check your infix string.There is a missing of closing bracket");
+        }else{
+            System.out.println("Corresponding Postfix String : "+result);
+        }
     }
 }
